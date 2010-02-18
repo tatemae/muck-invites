c20214b4b459a3849ab3da3550cf14675c0a638d
Justin Ball <justinball@gmail.com>
2010-02-17 17:08:32 -0700

    refactored rake task

--- lib/muck_invites/tasks.rb
+++ lib/muck_invites/tasks.rb
@@ -12,9 +12,9 @@ module MuckInvites
     def define
       
       namespace :muck do
-        namespace :invites do
+        namespace :sync do
           desc "Sync required files from muck invites."
-          task :sync do
+          task :invites do
             path = File.join(File.dirname(__FILE__), *%w[.. ..])
             system "rsync -ruv #{path}/db ."
             system "rsync -ruv #{path}/public ."
